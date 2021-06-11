5d7ce740f64774c01b2c0cb51e75d74d9a691b1f
Francesco Olivo <francesco.olivo99@gmail.com>
2021-06-11 11:15:15 +0200

    Aggiunto inserimento singola ricetta in RicettaDao

---
+++ app/src/main/java/it/unibo/android/ricettapp/persistence/Repository.kt
@@ -0,0 +1,55 @@
+package it.unibo.android.ricettapp.persistence
+
+import android.content.Context
+import androidx.lifecycle.LiveData
+import androidx.room.Room
+import it.unibo.android.ricettapp.model.Ricetta
+import java.util.concurrent.Executors
+
+private const val DATABASE_NAME = "ricettapp-database"
+
+class Repository private constructor(context: Context) {
+
+    private val database : AppDatabase = Room.databaseBuilder(
+        context.applicationContext,
+        AppDatabase::class.java,
+        DATABASE_NAME
+    ).build()
+
+    private val ricettaDao = database.ricettaDao()
+    private val executor = Executors.newSingleThreadExecutor()
+
+
+    fun getRicette(): LiveData<List<Ricetta>> = ricettaDao.all
+
+    fun getRicetta(id: Long): LiveData<Ricetta?> = ricettaDao.getRicetta(id)
+
+    fun aggiornaRicetta(ricetta: Ricetta) {
+        executor.execute {
+            ricettaDao.update(ricetta)
+        }
+    }
+
+    fun eliminaRicetta(ricetta: Ricetta) {
+        executor.execute {
+            ricettaDao.delete(ricetta)
+        }
+    }
+
+    companion object {
+        private var INSTANCE: Repository? = null
+
+        fun initialize(context: Context) {
+            if (INSTANCE == null) {
+                INSTANCE = Repository(context)
+            }
+        }
+
+        fun get(): Repository {
+            return INSTANCE ?:
+            throw IllegalStateException("Il repository deve essere inizializzato")
+        }
+
+    }
+
+}
--- app/src/main/java/it/unibo/android/ricettapp/persistence/RicettaDao.java
+++ app/src/main/java/it/unibo/android/ricettapp/persistence/RicettaDao.java
@@ -5,6 +5,7 @@ import androidx.room.Dao;
 import androidx.room.Delete;
 import androidx.room.Insert;
 import androidx.room.Query;
+import androidx.room.Update;
 
 import java.util.List;
 
@@ -16,8 +17,14 @@ public interface RicettaDao {
     @Query("SELECT * FROM ricetta")
     LiveData<List<Ricetta>> getAll();
 
+    @Query("SELECT * FROM ricetta WHERE id=(:id)")
+    LiveData<Ricetta> getRicetta(Long id);
+
     @Insert
     void insertAll(Ricetta... ricettas) ;
+
+    @Update
+    void update(Ricetta ricetta);
 
     @Delete
     void delete(Ricetta ricetta) ;
